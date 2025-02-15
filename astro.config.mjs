7c9bde929536b239e4cb0d73d9c88a8aaabf697b
jd-apprentice <contacto@jonathan.com.ar>
2025-02-15 16:03:05 -0300

    chore: first attempt to add sentry, license, readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+import sentry from '@sentry/astro';
 
 // https://astro.build/config
+// https://docs.astro.build/en/guides/environment-variables/
 export default defineConfig({
-    site: 'https://links.jonathan.com.ar',
+  site: 'https://links.jonathan.com.ar',
+  env: {
+    schema: {
+      SENTRY_AUTH_TOKEN: envField.string({
+        context: "server", optional: false,
+        access: 'secret'
+      }),
+    }
+  },
+  integrations: [sentry({
+    dsn: "https://a80e056db50a348719874ab38e1dbdcb@o4505914092093440.ingest.us.sentry.io/4508824451678208",
+    environment: import.meta.env.PROD ? "prod" : "unreleased",
+    release: process.env.npm_package_version,
+    sourceMapsUploadOptions: {
+      project: "jd-links",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }
+  })],
 });
